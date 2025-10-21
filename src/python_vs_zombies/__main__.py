462c23f0c5fa8206cf2fc3bcd28beb3fa0e213a5
parsariazitehrani <parsariazitehrani@gmail.com>
2025-10-21 11:29:23 -0700

    zombie_move,flower_attack

--- src/python_vs_zombies/__main__.py
+++ src/python_vs_zombies/__main__.py
@@ -1,6 +1,7 @@
 import os
 import time
-
+import pywinstyles
+from random import choice
 from tkinter import *
 from customtkinter import *
 from PIL import Image,ImageTk
@@ -10,46 +11,188 @@ def start_game():
     root.title("Python v.s Zombies")
     root.minsize(1024, 600)
     root.maxsize(1024, 600)
-    # root.columnconfigure(0, weight=2)
-    # root.columnconfigure(1, weight=3)
-    root.rowconfigure(0, weight=1)
-    root.rowconfigure(1, weight=2)
-    root.rowconfigure(3, weight=2)
-    root.rowconfigure(4, weight=2)
-    root.rowconfigure(5, weight=2)
-    root.rowconfigure(6, weight=1)
     # Load image bg
     base_dir= os.path.dirname(__file__)
     background_path= os.path.join(base_dir, "assets/images/background.png")
     image = PhotoImage(file=background_path)
     bg_image = CTkLabel(root, image=image, text="")
     bg_image.place(relheight=1, relwidth=1)
-    # test
-    # plant1= CTkLabel(root, text="flower")
-    # plant1.grid(row=2, column=2)
-    # zobmei1= CTkLabel(root, text="zombie") 
-    # zobmei1.grid(row=2, column= 11)
-    def movement(root, label):
-        move = 0
-        for i in range(10):
-            move += 1
-            label.grid(row= 2, column= 10, padx= move, pady= 1, sticky="w")
-            root.update()
-            time.sleep(0.2)
-    potato= CTkLabel(root, text="potato")
-    potato.grid(row= 2, column= 1, padx= 15, pady= 60, sticky="e")
-    btm = CTkButton(root, text="move!", command=lambda: movement(root, dadash))
-    btm.grid(row = 7)
-    dadash= CTkLabel(root, text="dada potato")
-    dadash.grid(row= 2, column= 10, padx= 190, pady= 1, sticky="w")
-    
-    # point= CTkLabel(root, text="point:20")
-    # point.grid(row=0, column=2)
-    # timeline= CTkProgressBar(root)
-    # timeline.grid(row= 0, column= 12)
 
-    #its too long and mess up grid
+    # planting
+    normal_font = CTkFont(family="arial bold", size=20)
+    cells_column = ((335, 83), (414, 83), (497, 83), (572, 83), (661, 83), (740, 83), (815, 83), (892, 83), (972, 83))
+    cells_row = ((254, 183), (254, 274), (254, 377), (254, 470), (254, 570))
+
+    class Plant:
+        def __init__(self, window, text):
+            self.label = CTkLabel(window, text = text, fg_color = "green2", text_color = "black")
+            self.damage = 40
+        def plant_place(self, x_size, y_size):
+            self.label.place(x= x_size, y=y_size)
+        def take_damage(self, amount):
+            self.damage -= amount
+            if self.damage == 0 :
+                    self.label.destroy()
+                    return False
+            if self.damage > 0 :
+                self.label.configure(fg_color = 'red')
+                time.sleep(1)
+    class Sunflower(Plant):
+        def __init__(self, window, text):
+            super().__init__(window, text)
+    class Bean(Plant):
+        def __init__(self, window, text):
+            super().__init__(window, text)
+        def attack(self, x_ball, y_ball):
+            self.start = x_ball
+            self.moving = self.start
+            ball = CTkLabel(root, text = "O", width = 12, height = 10)
+            ball.place(x = self.start - 60, y = y_ball - 80) 
+            outside = False
+            def fire():
+                nonlocal outside
+                self.moving += 3
+                print(self.moving)
+                ball.place(x = self.moving, y = y_ball - 70)
+                print(f"y = {y_ball}")
+                root.after(100,fire)
+                print("anjam")
+                for widget in root.winfo_children():
+                    if isinstance(widget, CTkLabel) and "zombie" in widget.cget("text").lower():
+                        zx, zy = widget.winfo_x(),widget.winfo_y()
+                        if zx - x_ball < 10 and zy - y_ball < 10:
+                            widget.damaged()
+            fire()
+    class Defensive(Plant):
+        def __init__(self, window, text):
+            super().__init__(window, text)
+
+
+
+    zombies_number = [(0, 183), (0, 274), (0, 377), (0, 470), (0, 570)]
+    class Zombie:
+        def __init__(self, window, text, garden):
+            self.lebel = CTkLabel(window, text = text)
+            self.garden = garden
+            self.start = 1050
+            self.moving = 4
+            self.x_moving = self.start
+        def spown(self, row):
+            self.lebel.place(x = 1050, y = row[1] - 65)
+
+        def move(self, row):
+            self.x_moving -= self.moving
+            # print(self.x_moving)
+            self.lebel.place(x = self.x_moving, y = row[1] - 65) 
+            row_index = zombies_number.index(tuple(row))
+            zombies_number[row[0]] = self
+            for plant in garden[row_index]:
+                if plant is not None:
+                    x_plant = garden[row_index].index(plant)                 
+                    column_plant = cells_column[x_plant][0]
+                    between = column_plant - self.x_moving
+                    if between >= 10:
+                        speed = self.attack(x_plant,row_index)
+                        if speed == False:
+                            time.sleep(2)
+                            self.moving = 4
+                        if speed:
+                            self.moving = 0                
+            root.after(100, self.move, row)
+        def attack(self,x_plant,y_plant):
+            enenmy = self.garden[y_plant][x_plant]
+            alive = enenmy.take_damage(20)
+            if alive == False:
+                return False
+        def damaged(self):
+            self.lebel.destroy()
     
+    # all blocks
+    garden = [
+        [None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None]
+    ]
+    # zombies***
+    # spown 
+    row_zombie = list(choice(zombies_number))
+    my_zom = Zombie(root, "zombie", garden)
+    my_zom.spown(row_zombie)
+    my_zom.move(row_zombie)
+
+
+    
+
+    # frame
+    flower_menu = CTkFrame(root, height = 250, width = 100, bg_color = "white", fg_color= "burlywood3") 
+    flower_menu.place(x = 50, y = 60)
+    flower_menu.pack_propagate(False)
+    # flower = Panel(flower_menu, "sunflower", "orange2", "black", [10, 10])
+    btm_sunflower = CTkButton(flower_menu, text = "sunflower", fg_color = "orange2", text_color="black", width = 80, height = 40, command = lambda : btm_clicked(btm_sunflower, Sunflower(root, "sunflower")))
+    btm_sunflower.place(x = 10, y = 10)
+    btm_potato = CTkButton(flower_menu, text= "potato", fg_color = "orangered2", text_color="black", width = 80, height = 40, command = lambda : btm_clicked(btm_potato, Defensive(root, "potato")))
+    btm_potato.place(x = 10, y = 55)
+    btm_bean = CTkButton(flower_menu, text = "beans",fg_color = "springgreen2", text_color="black", width = 80, height = 40, command = lambda : btm_clicked(btm_bean, Bean(root, "bean")))
+    btm_bean.place(x = 10, y = 100)
+    btm_pepper = CTkButton(flower_menu, text ="pepper", fg_color = "firebrick2", text_color="black", width = 80, height = 40, command = lambda : btm_clicked(btm_pepper, Defensive(root, "pepper")))
+    btm_pepper.place(x = 10, y = 145 )
+    btm_list = [btm_sunflower, btm_potato, btm_bean, btm_pepper]
+    flower = None
+    clicked = True
+    def btm_clicked(flower_btm, plant):
+        nonlocal clicked
+        nonlocal flower
+        nonlocal row_zombie
+        flower = plant
+        clicked = False
+        for btm in btm_list:
+            if btm != flower_btm:
+                btm.configure(state = "disabled")
+        if clicked == False:
+            bg_image.bind("<Button-1>", garden_clicked)
+        else:
+            print("you have to clicked one button")
+        
+    def garden_clicked(event):
+        nonlocal flower
+        nonlocal clicked
+        print(f"Mouse pointer coordinates: X={event.x}, Y={event.y}")
+        x_mouse, y_mouse = event.x, event.y
+        for cell in cells_column:
+            x_cell, y_cell = cell
+            if x_cell > x_mouse:
+                column = cells_column.index(cell)
+                break
+        else:
+            print("you clicked out of garden")
+        for cell in cells_row:
+            x_cell, y_cell = cell
+            if y_cell > y_mouse:
+                row = cells_row.index(cell)
+                break
+        else:
+            print("you clicked out of garden") 
+        
+        x_label = cells_column[column]
+        y_lebel = cells_row[row]
+        print(row, column)
+        if garden[row][column] is None:
+            flower.plant_place(x_label[0] - 60, y_lebel[1] - 70)
+            print(clicked)
+            garden[row][column] = flower
+            for i in btm_list:
+                i.configure(state = "abled")
+        print(row_zombie,y_lebel)
+        if y_lebel[1] == row_zombie[1]:
+            try:
+                flower.attack(x_label[0], y_lebel[1])
+            except:
+                print("this flower is defensive")
+
+        clicked = True
+        
     root.mainloop()
 
 def start_game_pressed(window):
